5bd2c9ca93e4c0877abca9648698c624d83794b2
Leonardo Liao <=>
2020-06-29 22:35:36 +1000

    add local address support

--- index.d.ts
+++ index.d.ts
@@ -71,6 +71,7 @@ export interface AxiosRequestConfig {
   proxy?: AxiosProxyConfig | false;
   cancelToken?: CancelToken;
   decompress?: boolean;
+  localAddress?: string;
 }
 
 export interface AxiosResponse<T = any>  {
